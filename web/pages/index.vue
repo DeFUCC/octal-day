80e06e430c440915cc589846258513ebb9808eb0
davay <davay@defucc.me>
2026-08-02 10:32:02 +0700

    feat: day is derived

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import DayLine from './line.vue'
-import { transits } from '../../src/astro.js'
+import { transits, octaeteride, seasonCount, dayCount } from '../../src/astro.js'
 import { computed } from 'vue'
 import { now, astro, octaDays, dayFraction, octalDayFraction as oct, levels, colors, coord, arrows, trigrams, octime, } from '../../src/useDay.js';
 
@@ -11,7 +11,9 @@ import { now, astro, octaDays, dayFraction, octalDayFraction as oct, levels, col
 <template lang="pug">
 section.bg-yellow-50.flex.flex-col.gap-2.p-2.overflow-y-scroll
   .flex.gap-2.justify-between
-    .font-mono 
+    .font-mono {{octaeteride.toString(8)}}-{{seasonCount.toString(8)}}-{{(dayCount-seasonCount*73).toString(8)}}
+    .flex-1
+    .font-bold {{dayCount%8}} 
     .flex-1
     .font-mono {{octime}}
   .flex.flex-col.gap-2.justify-around.shadow-lg.rounded-lg(v-for="(level,l) in levels" :key="level" )
@@ -22,10 +24,12 @@ section.bg-yellow-50.flex.flex-col.gap-2.p-2.overflow-y-scroll
       .text-2xl {{arrows[oct[l]]}}
       .flex-1 
       .text-2xl {{trigrams[oct[l]]}}
-    .flex
-      .p-1.m-1.flex-1.transition.border-1.border-dark-100.border-op-50.shadow.shadow-inset.rounded-xl(v-for="(row,r) in 8" :style="{backgroundColor:r==oct[l]?colors[oct[l]]:'transparent'}")
-    .p-2.bg-light-200.flex.flex-col.gap-2
+    
+    .p-2.bg-light-200.flex.gap-2
       .op-30.text-xs {{Number('0o'+oct.slice(0,l+1))}}/{{Math.pow(8,l+1)}}
+      .flex-1
+      .flex
+        .p-1.m-1.flex-1.transition.border-1.border-dark-100.border-op-50.shadow.shadow-inset.rounded-xl(v-for="(row,r) in 8" :style="{backgroundColor:r==oct[l]?colors[oct[l]]:'transparent'}")
 
 
 </template>
