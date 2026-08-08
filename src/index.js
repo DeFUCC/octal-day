18b695d0d11a7342e33eac630eb6a07616115d63
davay <davay@defucc.me>
2026-08-08 17:16:04 +0700

    feat: polish

--- src/index.js
+++ src/index.js
@@ -1,2 +1,71 @@
-export * from './astro.js'
-export * as astronomy from 'astronomy-engine'
+// `octal-day` - planetary time scale 
+
+const VENUS_T0 = new Date(`1761-06-06T05:19:00Z`)  // -6581846460000 
+const VENUS_T1 = new Date(`1769-06-03T22:25:00Z`) // -6329583300000
+const UNIX_EPOCH = new Date(`1970-01-01T00:00:00Z`) // 0
+const UNIX_DAY = new Date(`1970-01-02T00:00:00Z`) // 86400000
+const VENUS_T2 = new Date(`2004-06-08T08:20:00Z`) // 1086682800000
+const VENUS_T3 = new Date(`2012-06-06T01:29:00Z`) // 1338946140000
+const VENUS_T4 = new Date(`2247-06-11T11:33:00Z`) // 8755212780000
+const VENUS_T5 = new Date(`2255-06-09T04:38:00Z`) // 9007475880000
+
+const T3_DAY = new Date(VENUS_T3).setUTCHours(0, 0, 0, 0) // 1338940800000
+const T5_DAY = new Date(VENUS_T5).setUTCHours(0, 0, 0, 0) // 9007459200000
+
+const ERA_DAYS = (T5_DAY - T3_DAY) / UNIX_DAY // 88756 === 0o255264
+const TRANSIT_DAYS = Math.ceil((VENUS_T3 - VENUS_T2) / UNIX_DAY) // 2920 === 0o5550
+const SUN = 8 // 0o555x0o10=0o5550
+const VENUS = 5 // 0o1110x0o5=0o5550
+const PETAL = TRANSIT_DAYS / (SUN * VENUS)  // 2920/40=73 === 0o5550/0o50=0o111
+
+export function useDay(timestamp = Date.now(), offset = (new Date().getTimezoneOffset() * 60 * 1000) / UNIX_DAY) {
+
+  if (!timestamp) return {}
+
+  const octal = {}
+  const render = {}
+
+  const utcDaysSinceEpoch = (timestamp - T3_DAY) / UNIX_DAY;
+  const adjustedDays = utcDaysSinceEpoch - offset
+
+  const eraIndex = Math.floor(adjustedDays / ERA_DAYS)
+  const eraDayRaw = adjustedDays - (eraIndex * ERA_DAYS);
+  const normalizedEraDay = ((eraDayRaw % ERA_DAYS) + ERA_DAYS) % ERA_DAYS;
+  const transit = Math.floor(normalizedEraDay / TRANSIT_DAYS)
+  const transitDay = Math.floor(normalizedEraDay) % TRANSIT_DAYS
+  const petal = Math.floor(transitDay / PETAL)
+  const petalOctave = Math.floor((transitDay - PETAL * petal) / 8)
+  const sun = Math.floor(petal / 5)
+  const sunPetal = petal - sun * 5
+  const venus = Math.floor(petal / 8)
+  const venusPetal = petal - venus * 8
+
+  const raw = { era: eraIndex, day: normalizedEraDay }
+
+  octal.era = (eraIndex + 1).toString(8)
+  octal.eraDay = Math.floor(normalizedEraDay).toString(8)
+  octal.time = (normalizedEraDay % 1).toString(8).padEnd(6, '0').slice(2)
+  octal.transit = transit.toString(8)
+  octal.transitDay = transitDay.toString(8)
+  octal.transitOctave = transitDay.toString(8).slice(0, -1)
+  octal.octave = octal.eraDay.slice(0, -1)
+  octal.octaveDay = octal.eraDay.slice(-1)
+  octal.petal = petal.toString(8)
+  octal.sun = sun.toString(8)
+  octal.sunPetal = sunPetal.toString(8)
+  octal.petalOctave = petalOctave.toString(8)
+
+  render.time = `${octal.time.slice(0, 2)}:${octal.time.slice(2, 4)}:${octal.time.slice(4, 6)}`
+  render.era = 'E' + octal.era + '-D' + octal.eraDay.padStart(6, '0')
+  render.transit = `e${octal.era}-t${octal.transit}-d${octal.transitDay}`
+  render.octave = `e${octal.era}-t${octal.transit}-v${octal.transitOctave}-d${octal.octaveDay}`
+  render.petal = `e${octal.era}-t${octal.transit}-p${octal.petal}-v${octal.petalOctave}-d${octal.octaveDay}`
+  render.sun = `e${octal.era}-t${octal.transit}-s${octal.sun}-p${octal.sunPetal}-v${petalOctave.toString(8)}-d${octal.octaveDay}`
+  render.venus = `e${octal.era}-t${octal.transit}-n${venus.toString(8)}-p${venusPetal.toString(8)}-v${petalOctave.toString(8)}-d${octal.octaveDay}`
+
+  return { raw, octal, render }
+}
+
+export function useDate({ count, time = 0, era = 1 } = {}) {
+  return new Date(T3_DAY + ((era - 1) * ERA_DAYS + (parseInt(count, 8) + parseInt(time, 8) / (8 ** time.length))) * UNIX_DAY)
+}
--- web/pages/octave.vue
+++ web/pages/octave.vue
@@ -1,77 +1,10 @@
 <script setup>
 import { useNow } from '@vueuse/core'
 import { computed, ref } from 'vue'
-
-
-const T0 = new Date(`1761-06-06T05:19:00Z`)
-const T1 = new Date(`1769-06-03T22:25:00Z`)
-const UNIX_EPOCH = new Date(`1970-01-01T00:00:00Z`)
-const UNIX_DAY = new Date(`1970-01-02T00:00:00Z`)
-const JD2000 = new Date(`2000-01-01T11:58:55.816Z`)
-const T2 = new Date(`2004-06-08T08:20:00Z`)
-const T3 = new Date(`2012-06-06T01:29:00Z`)
-const today = new Date()
-const T4 = new Date(`2247-06-11T11:33:00Z`)
-const T5 = new Date(`2255-06-09T04:38:00Z`)
-
-const T3_DAY = new Date(T3).setHours(0, 0, 0, 0)
-const T5_DAY = new Date(T5).setHours(0, 0, 0, 0)
-const ERA_DAYS = (T5_DAY - T3_DAY) / UNIX_DAY // exactly 88756 or 0o255264 in octal
-const OCTAETERIS = Math.ceil((T3 - T2) / UNIX_DAY) // exactly 2920 - 0o5550 in octal
-const SUNS = 8 // 2920/365 - 0o555 in octal
-const VENUSES = 5 //2920/485 - 0o1110 in octal
-const PETAL = OCTAETERIS / (SUNS * VENUSES) // exactly 73, 0o111 - octal repunit of order 3
-
-function useDay(date) {
-
-  const raw = { OCTAETERIS, ERA_DAYS }, octal = {}, render = {}
-  if (!date) return { raw }
-  raw.date = date
-  raw.timestamp = new Date(date).getTime()
-  raw.ut = (raw.timestamp - JD2000) / UNIX_DAY
-  raw.count = (new Date(date) - T3_DAY) / UNIX_DAY
-  raw.days = ((raw.count % ERA_DAYS) + ERA_DAYS) % ERA_DAYS
-  raw.era = Math.floor(raw.days / ERA_DAYS) + 1
-  raw.int = Math.floor(raw.days)
-  raw.transit = Math.floor(raw.int / OCTAETERIS)
-  raw.transitDay = raw.int % OCTAETERIS
-  raw.petal = Math.floor(raw.transitDay / PETAL)
-  raw.petalOctave = Math.floor((raw.transitDay - PETAL * raw.petal) / 8)
-  raw.sun = Math.floor(raw.petal / 5)
-  raw.sunPetal = raw.petal - raw.sun * 5
-  raw.venus = Math.floor(raw.petal / 8)
-  raw.venusPetal = raw.petal - raw.venus * 8
-  raw.progress = raw.days / ERA_DAYS
-
-  octal.era = raw.era.toString(8)
-  octal.day = raw.days.toString(8)
-  octal.count = raw.int.toString(8)
-  octal.time = (raw.days % 1).toString(8).slice(2)
-  octal.transit = raw.transit.toString(8)
-  octal.transitDay = raw.transitDay.toString(8)
-  octal.transitOctave = raw.transitDay.toString(8).slice(0, -1)
-  octal.octave = octal.count.slice(0, -1)
-  octal.octaveDay = octal.count.slice(-1)
-  octal.petal = raw.petal.toString(8)
-  octal.petalOctave = raw.petalOctave.toString(8)
-
-  octal.date = 'E' + octal.era + '-D' + octal.count.padStart(6, '0')
-
-  render.transit = `e${octal.era}-t${octal.transit}-d${octal.transitDay}`
-  render.octave = `e${octal.era}-t${octal.transit}-v${octal.transitOctave}-d${octal.octaveDay}`
-  render.petal = `e${octal.era}-t${octal.transit}-p${octal.petal}-v${octal.petalOctave}-d${octal.octaveDay}`
-  render.sun = `e${octal.era}-t${octal.transit}-s${raw.sun.toString(8)}-p${raw.sunPetal.toString(8)}-v${raw.petalOctave.toString(8)}-d${octal.octaveDay}`
-  render.venus = `e${octal.era}-t${octal.transit}-n${raw.venus.toString(8)}-p${raw.venusPetal.toString(8)}-v${raw.petalOctave.toString(8)}-d${octal.octaveDay}`
-
-  return { raw, octal, render }
-}
-
-function useDate({ count, time = 0, era = 1 } = {}) {
-  return new Date(T3_DAY + ((era - 1) * ERA_DAYS + (parseInt(count, 8) + parseInt(time, 8) / (8 ** time.length))) * UNIX_DAY)
-}
+import { useDay, useDate } from '../../src/index.js'
 
 const now = useNow()
-const day = computed(() => useDay(now.value))
+const day = computed(() => useDay(now.value.getTime()))
 const inputDate = ref()
 const inputDay = ref('034422')
 const inputTime = ref('236533')
@@ -79,15 +12,12 @@ const inputTime = ref('236533')
 </script>
 
 <template lang="pug">
-section.p-8.bg-green-100.w-full.h-full.overflow-y-scroll
-  h2.text-xl.font-bold Octave 
-  .flex
-    pre {{day}}
+section.p-8.bg-green-100.w-full.h-full.overflow-y-scroll.flex.flex-col.gap-2
+    h1.text-2xl.font-mono.font-bold  {{day.render.era}}.{{day.render.time}}
+    h2.text-xl.font-bold Octave: {{day.render.octave}}
+    h2.text-xl.font-bold Petal: {{day.render.petal}}
+    h2.text-xl.font-bold Sun: {{day.render.sun}}
+    h2.text-xl.font-bold Venus: {{day.render.venus}}
 
-  input(type="date" v-model="inputDate")
-  pre {{useDay(inputDate)}}
-  pre {{useDate(useDay(inputDate))}}
-  input(v-model="inputDay")
-  input(v-model="inputTime")
-  pre {{useDate({day:inputDay, time:inputTime})}}
+
 </template>
