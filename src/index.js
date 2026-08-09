f98f8687fe5c1fdda812db0f89021f7d424cbd7f
davay <davay@defucc.me>
2026-08-10 01:19:06 +0700

    feat: compact

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ export const useDay = (ms = Date.now(), tz = new Date().getTimezoneOffset() / 14
   return `${era.toString(8)}-${Math.floor(rem / TRANSIT_DAYS).toString(8).padStart(2, '0')}-${(rem % TRANSIT_DAYS).toString(8).padStart(4, '0')} ${o.slice(0, 2)}:${o.slice(2, 4)}:${o.slice(4, 6)}`;
 };
 
-export const useDate = (s, tz = new Date().getTimezoneOffset() / 1440, epoch = 1338940800000, eraOffset = 1) => {
-  let [dt, tm] = s.split(' '), [e, t, d] = dt.split('-'), [h, m, sc] = tm.split(':');
+export const useDate = (day, tz = new Date().getTimezoneOffset() / 1440, epoch = 1338940800000, eraOffset = 1) => {
+  let [dt, tm] = day.split(' '), [e, t, d] = dt.split('-'), [h, m, sc] = tm.split(':');
   let era = parseInt(e, 8) - eraOffset - 1;
   return new Date(((era * ERA_DAYS + parseInt(t, 8) * TRANSIT_DAYS + parseInt(d, 8) + parseInt(h + m + sc, 8) / TIME_SCALE) + tz) * DAY_MS + epoch);
 };
