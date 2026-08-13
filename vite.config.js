0d5a4e9e6bc899d9053bd048df7bd969451f837b
davay <davay@defucc.me>
2026-08-13 18:15:49 +0700

    feat: build

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
     port: '4222'
   },
   plugins: [
+    vue(),
     VueRouter({
       routesFolder: [
         { src: "src/pages" }
@@ -45,7 +46,7 @@ export default defineConfig({
       extractors: [extractorSplit, extractorPug()],
     }),
     viteSingleFile(),
-    vue(),
+    viteBuildScript(),
   ],
 })
 
