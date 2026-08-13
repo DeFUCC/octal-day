e4327200f57bd71e9e8efd30b91529498807afd1
davay42 <davay42@users.noreply.github.com>
2026-08-13 11:08:31 +0000

    deploy: 300aefdd0594b9c63fecaf3df55d5aaeb7dee328

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'octal-day-v.0.8.8'
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/logo.svg',
+]
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim()
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request)
+    })
+  );
+});
